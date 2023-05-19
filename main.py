c91523de8969d819cb83a96583c0f39cce1e2e16
SVafadar69 <svafadar19@gmail.com>
2023-05-19 00:24:57 -0600

    w

--- main.py
+++ main.py
@@ -3,34 +3,30 @@ import time
 import keyboard
 import os
 from playsound import playsound
+import screen_brightness_control as sbc
 
-# Define the Windows API functions
-SetMonitorBrightness = ctypes.windll.dxva2.SetMonitorBrightness
-GetPhysicalMonitorsFromHMONITOR = ctypes.windll.dxva2.GetPhysicalMonitorsFromHMONITOR
-GetMonitorBrightness = ctypes.windll.dxva2.GetMonitorBrightness
-LockWorkStation = ctypes.windll.user32.LockWorkStation
 
 # Constants
-NULL = 0
-MONITOR_DEFAULTTOPRIMARY = 1
+brightness = sbc.get_brightness()
+# set the brightness to X % for the primary monitor
+sbc.set_brightness(100, display=0)
+sbc.set_brightness(100, display=1)
 BRIGHTNESS_DIM = 0
-BREAK_INTERVAL = 20 * 60  # 20 minutes in seconds
+BREAK_INTERVAL = 1 * 60  # 20 minutes in seconds
 
-# Get the handle to the primary monitor
-hMonitor = ctypes.windll.user32.MonitorFromWindow(NULL, MONITOR_DEFAULTTOPRIMARY)
-
-# Get the physical monitors from the handle
-physical_monitor_array = ctypes.POINTER(ctypes.c_void_p)()
-GetPhysicalMonitorsFromHMONITOR(hMonitor, 1, ctypes.byref(physical_monitor_array))
-
-# Get the handle to the first physical monitor
-hPhysicalMonitor = physical_monitor_array.contents.value
 
 # Define the path to the background music file
-music_file_path = "path/to/your/music/file.mp3"
+music_file_path = r"C:\Users\svafa\Downloads\Marconi Union - Weightless (Official Video).mp3"
+
+# show brightness for each detected monitor
+monitors = sbc.list_monitors()
+print("monitors", monitors)
 
 # Function to lock the keys
 def lock_keys():
+    keyboard.block_all()
+
+def unlock_keys():
     keyboard.block_all()
 
 # Function to play background music
@@ -39,30 +35,39 @@ def play_music():
 
 # Function to dim the screen brightness
 def dim_screen():
-    SetMonitorBrightness(hPhysicalMonitor, BRIGHTNESS_DIM)
+    sbc.fade_brightness(0, display=0)
+    sbc.fade_brightness(0, display=1)
 
-# Main loop
-while True:
-    # Dim the screen brightness
-    dim_screen()
+def get_current_brightness():
+    x = sbc.get_brightness(display=0)
+    y = sbc.get_brightness(display=1)
 
-    # Lock the keys
-    lock_keys()
+    return x, y
 
-    # Play background music
-    play_music()
-
-    # Wait for the break interval
-    time.sleep(BREAK_INTERVAL)
-
-    # Restore the screen brightness
-    SetMonitorBrightness(hPhysicalMonitor, 100)
-
-    # Unblock the keys
-    keyboard.unblock_all()
-
-    # Stop the background music
-    os.system("taskkill /im wmplayer.exe /f")  # Terminate Windows Media Player process
-
-    # Wait for a few seconds before starting the next break interval
-    time.sleep(5)
+dim_screen()
+print(get_current_brightness())
+# # Main loop
+# while True:
+#     # Dim the screen brightness
+#     dim_screen()
+#
+#     # Lock the keys
+#     lock_keys()
+#
+#     # Play background music
+#     play_music()
+#
+#     # Wait for the break interval
+#     time.sleep(BREAK_INTERVAL)
+#
+#     # Restore the screen brightness
+#     SetMonitorBrightness(hPhysicalMonitor, 100)
+#
+#     # Unblock the keys
+#     unlock_keys()
+#
+#     # Stop the background music
+#     os.system("taskkill /im wmplayer.exe /f")  # Terminate Windows Media Player process
+#
+#     # Wait for a few seconds before starting the next break interval
+#     time.sleep(5)
